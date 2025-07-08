84cc0de8ed790c9bb591e153c35f0dcac64c6097
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-08 16:56:53 +0200

    Initial commit

---
+++ src/test/java/com/formicola/wheater/WheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formicola.wheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
